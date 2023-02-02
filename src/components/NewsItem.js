6b0cd99fbef5548ebbc01c6c881e6c574fbc4a43
Rishabh Verma <86519740+rishabhv12@users.noreply.github.com>
2023-02-03 00:11:09 +0530

    update descrption item

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -17,7 +17,7 @@ const NewsItem = (props)=> {
                     <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}  </h5>
-                        <p className="card-text">{description}</p>
+                        {description}
                         <p className="card-text"><small className="text-danger">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
